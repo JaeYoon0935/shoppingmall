31d38d80cdc6930e26f37e4038a6685b1de3a63c
JaeYoon <taswp@naver.com>
2020-11-30 08:49:42 +0900

    수정 취소 기능구현

--- src/shoppingmall-vue/src/views/User/UserList.vue
+++ src/shoppingmall-vue/src/views/User/UserList.vue
@@ -7,7 +7,7 @@
         :items-per-page="10"
         class="elevation-1"
       >
-      <template v-slot:item="row" >
+      <template v-if=temp v-slot:item="row">
           <tr>     
             <td>{{row.item.username}}</td>
             <td>{{row.item.password}}</td>
@@ -16,12 +16,29 @@
             <td>{{row.item.phone}}</td>
             <td>{{row.item.email}}</td>
             <td>{{row.item.point}}</td>
-
             <td>
-            <!-- <v-card-actions class="justify-center">   -->
               <v-card-actions class="justify-start">  
-                <v-btn dark small color="grey" @click="onButtonClick(row.item)">수정</v-btn>
+                <v-btn dark small color="grey" @click="Update(row.item)">수정</v-btn>
                 <v-btn dark small color="grey" @click="UserDelete(row.item)">탈퇴</v-btn>
+              </v-card-actions>
+            </td>
+          </tr>
+      </template>
+
+      <!-- 수정버튼을 눌렀을 때, 해당 행만 바뀌도록 하면 됨, -->
+      <template v-else v-slot:item="row" :index="1">
+          <tr>     
+            <td><input v-model="inputText"></td>
+            <td>{{row.item.password}}</td>
+            <td>{{row.item.name}}</td>
+            <td>{{row.item.address}}</td>
+            <td>{{row.item.phone}}</td>
+            <td>{{row.item.email}}</td>
+            <td>{{row.item.point}}</td>
+            <td>
+              <v-card-actions class="justify-start">  
+                <v-btn dark small color="grey">수정완료</v-btn>
+                <v-btn dark small color="grey" @click="Updata_cancle()">취소</v-btn>
               </v-card-actions>
             </td>
           </tr>
@@ -38,10 +55,18 @@ import { mapState, mapActions } from "vuex"
     },
     data () {
       return {     
+        temp: true,
+        inputText: ''
       }
     },
     methods:{
-        ...mapActions(["UserDelete"])
+        ...mapActions(["UserDelete"]),
+        Update: function(){
+        this.temp=false
+        },
+        Updata_cancle: function(){
+        this.temp=true  
+        }
     },
   }
 </script>
