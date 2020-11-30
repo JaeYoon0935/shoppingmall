568d4cc831edae26a446d6502fbf91e5eb69d401
ChoJaeYoon <taswp@naver.com>
2020-11-30 18:35:55 +0900

    input버튼 css로 너비조정 완료

--- src/shoppingmall-vue/src/views/User/UserList.vue
+++ src/shoppingmall-vue/src/views/User/UserList.vue
@@ -4,37 +4,65 @@
      <v-data-table
       class="elevation-1"
        :headers="userlist_headers"
-       :items="userlist"  
+       :items="userlist"
      >
         <template v-slot:item="row">
          <tr>     
             <td>
-              <span v-if="temp == row.item.username"><input v-model="inputText"></span>
+              <span v-if="temp == row.item.username">
+                <input :style="{width:'82px'}" v-model="username">
+              </span>
               <span v-else>{{row.item.username}}</span>
             </td>
             <td>
-              <span v-if="temp == row.item.username"><input v-model="inputText"></span>
+              <span v-if="temp == row.item.username">
+                <input :style="{width:'95px'}" v-model="password">
+              </span>
               <span v-else>{{row.item.password}}</span>
             </td>
-            <td><span v-if="temp == row.item.username"><input v-model="inputText"></span>
-              <span v-else>{{row.item.name}}</span></td>
-            <td><span v-if="temp == row.item.username"><input v-model="inputText"></span>
-              <span v-else>{{row.item.address}}</span></td>
             <td>
-              <span v-if="temp == row.item.username"><input v-model="inputText"></span>
+              <span v-if="temp == row.item.username">
+                <input :style="{width:'69px'}" v-model="name">
+              </span>
+              <span v-else>{{row.item.name}}</span>
+            </td>
+            <td>
+              <span v-if="temp == row.item.username">
+                <input :style="{width:'104px'}" v-model="address">
+              </span>
+              <span v-else>{{row.item.address}}</span>
+            </td>
+            <td>
+              <span v-if="temp == row.item.username">
+                <input :style="{width:'107px'}" v-model="phone">
+              </span>
               <span v-else>{{row.item.phone}}</span>
             </td>
-            <td><span v-if="temp == row.item.username"><input v-model="inputText"></span>
-              <span v-else>{{row.item.email}}</span></td>
             <td>
-              <span v-if="temp == row.item.username"><input v-model="inputText"></span>
+              <span v-if="temp == row.item.username">
+                <input :style="{width:'131px'}" v-model="email">
+              </span>
+              <span v-else>{{row.item.email}}</span>
+            </td>
+            <td>
+              <span v-if="temp == row.item.username">
+                <input :style="{width:'83px'}" v-model="point">
+              </span>
               <span v-else>{{row.item.point}}</span>
             </td>
             <td>
+              <span v-if="temp != row.item.username">
               <v-card-actions class="justify-start">  
                 <v-btn dark small color="grey" @click="Update(row.item.username)">수정</v-btn>
                 <v-btn dark small color="grey" @click="UserDelete(row.item)">탈퇴</v-btn>
               </v-card-actions>
+              </span>
+              <span v-else>
+                <v-card-actions class="justify-start">  
+                <v-btn dark small color="grey">수정완료</v-btn>
+                <v-btn dark small color="grey" @click="Updata_cancle()">취소</v-btn>
+              </v-card-actions>
+              </span>
             </td>
           </tr>
         </template>
@@ -51,8 +79,13 @@ import { mapState, mapActions } from "vuex"
     data () {
       return {     
         temp:0,
-        index: 100,
-        inputText: ''
+        username: '',
+        password: '',
+        name:'',
+        address:'',
+        phone:'',
+        email:'',
+        point:''
       }
     },
     computed: {
@@ -61,9 +94,7 @@ import { mapState, mapActions } from "vuex"
     methods:{
         ...mapActions(["UserDelete"]),
         Update(username) {
-        // this.index = i
          this.temp=username
-        //alert(username)
         },
         Updata_cancle: function(){
         this.temp=true  
