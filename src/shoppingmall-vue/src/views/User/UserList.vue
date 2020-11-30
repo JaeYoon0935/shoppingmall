5d6c0f7ac1869c424f49eebf2657b6ee4e37505d
ChoJaeYoon <taswp@naver.com>
2020-11-30 17:51:06 +0900

    회원관리 행으로 수정되도록 구현

--- src/shoppingmall-vue/src/views/User/UserList.vue
+++ src/shoppingmall-vue/src/views/User/UserList.vue
@@ -1,49 +1,45 @@
 <template>
   <div class="about">
     <h1>회원관리 페이지입니다.</h1>
-      <v-data-table
-        :headers="$store.state.userlist_headers"
-        :items="$store.state.userlist"
-        :items-per-page="10"
-        class="elevation-1"
-      >
-      <template v-if=temp v-slot:item="row">
-          <tr>     
-            <td>{{row.item.username}}</td>
-            <td>{{row.item.password}}</td>
-            <td>{{row.item.name}}</td>
-            <td>{{row.item.address}}</td>
-            <td>{{row.item.phone}}</td>
-            <td>{{row.item.email}}</td>
-            <td>{{row.item.point}}</td>
+     <v-data-table
+      class="elevation-1"
+       :headers="userlist_headers"
+       :items="userlist"
+       
+     >
+        <template v-slot:item="row">
+         <tr>     
+            <td>
+              <span v-if="temp == row.item.username"><input v-model="inputText"></span>
+              <span v-else>{{row.item.username}}</span>
+            </td>
+            <td>
+              <span v-if="temp == row.username"><input v-model="inputText"></span>
+              <span v-else>{{row.password}}</span>
+            </td>
+            <td><span v-if="temp == row.username"><input v-model="inputText"></span>
+              <span v-else>{{row.name}}</span></td>
+            <td><span v-if="temp == row.username"><input v-model="inputText"></span>
+              <span v-else>{{row.address}}</span></td>
+            <td>
+              <span v-if="temp == row.username"><input v-model="inputText"></span>
+              <span v-else>{{row.phone}}</span>
+            </td>
+            <td><span v-if="temp == row.username"><input v-model="inputText"></span>
+              <span v-else>{{row.email}}</span></td>
+            <td>
+              <span v-if="temp == row.username"><input v-model="inputText"></span>
+              <span v-else>{{row.point}}</span>
+            </td>
             <td>
               <v-card-actions class="justify-start">  
-                <v-btn dark small color="grey" @click="Update(row.item)">수정</v-btn>
+                <v-btn dark small color="grey" @click="Update(row.item.username)">수정</v-btn>
                 <v-btn dark small color="grey" @click="UserDelete(row.item)">탈퇴</v-btn>
               </v-card-actions>
             </td>
           </tr>
-      </template>
-
-      <!-- 수정버튼을 눌렀을 때, 해당 행만 바뀌도록 하면 됨, -->
-      <template v-else v-slot:item="row" :index="1">
-          <tr>     
-            <td><input v-model="inputText"></td>
-            <td>{{row.item.password}}</td>
-            <td>{{row.item.name}}</td>
-            <td>{{row.item.address}}</td>
-            <td>{{row.item.phone}}</td>
-            <td>{{row.item.email}}</td>
-            <td>{{row.item.point}}</td>
-            <td>
-              <v-card-actions class="justify-start">  
-                <v-btn dark small color="grey">수정완료</v-btn>
-                <v-btn dark small color="grey" @click="Updata_cancle()">취소</v-btn>
-              </v-card-actions>
-            </td>
-          </tr>
-      </template>
-      </v-data-table>
+        </template>
+     </v-data-table>
   </div>
 </template>
 
@@ -55,14 +51,20 @@ import { mapState, mapActions } from "vuex"
     },
     data () {
       return {     
-        temp: true,
+        temp:0,
+        index: 100,
         inputText: ''
       }
     },
+    computed: {
+      ...mapState(["userlist","userlist_headers"])
+    },
     methods:{
         ...mapActions(["UserDelete"]),
-        Update: function(){
-        this.temp=false
+        Update(username) {
+        // this.index = i
+         this.temp=username
+        //alert(username)
         },
         Updata_cancle: function(){
         this.temp=true  
