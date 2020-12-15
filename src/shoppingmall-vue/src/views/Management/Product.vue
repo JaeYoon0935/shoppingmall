35fdab329b3528e41871458dd295c198873c74a1
ChoJaeYoon <taswp@naver.com>
2020-12-15 17:23:05 +0900

    상품관리 DB에 있는 제품 이미지등록

--- src/shoppingmall-vue/src/views/Management/Product.vue
+++ src/shoppingmall-vue/src/views/Management/Product.vue
@@ -16,13 +16,13 @@
             <td style="width:400px;">
                 <v-row style="display:flex; width:400px; align-items:center;"> 
                   <v-col style="overflow:hidden;">
-                    <img v-if="row.item.name=='방등'" :src="image.roomLamp" width="120%" />
-                    <img v-else-if="row.item.name=='거실등'" :src="image.livingLamp40" width="120%" />
-                    <!-- <img v-else-if="row.item.name=='거실등'" :src="image.livingLamp40" width="120%" />
-                    <img v-else-if="row.item.name=='거실등'" :src="image.livingLamp40" width="120%" />
-                    <img v-else-if="row.item.name=='거실등'" :src="image.livingLamp40" width="120%" />
-                    <img v-else-if="row.item.name=='거실등'" :src="image.livingLamp40" width="120%" />
-                    <img v-else-if="row.item.name=='거실등'" :src="image.livingLamp40" width="120%" /> -->
+                    <img v-if="row.item.name=='방등'" :src="image.roomLamp" width="90%" height="100px" />
+                    <img v-else-if="row.item.name=='거실등'" :src="image.livingLamp40" width="90%" height="100px" />
+                    <img v-else-if="row.item.name=='식탁등'" :src="image.diningLamp" width="90%" height="100px" />
+                    <img v-else-if="row.item.name=='형광등 스탠드'" :src="image.lightStand" width="90%" height="100px" />
+                    <img v-else-if="row.item.name=='LED 스탠드'" :src="image.ledStand" width="90%" height="100px" />
+                    <img v-else-if="row.item.name=='2구 스위치'" :src="image.switchTwo" width="90%" height="100px" />
+                    <img v-else-if="row.item.name=='멀티탭'" :src="image.multiPlug" width="90%" height="100px" />               
                   </v-col>
                   <v-col>
                     <div>상품명: {{row.item.name}}</div>
@@ -38,13 +38,13 @@
             <td>{{row.item.quantity}}개</td>
             <td>
                 <div>
-                  <v-btn dark small color="grey" class="ma-1" @click="Update(row.item)">수정</v-btn>
+                  <v-btn dark small color="grey" class="ma-2" @click="Update(row.item)">수정</v-btn>
                 </div>
                 <div>
-                 <v-btn dark small color="grey" class="ml-1" @click="UserDelete(row.item)">상품보기</v-btn>
+                 <v-btn dark small color="grey" class="ml-2" @click="UserDelete(row.item)">상품보기</v-btn>
                 </div>
                 <div>
-                  <v-btn dark small color="grey" class="ma-1" @click="UserDelete(row.item)">삭제</v-btn>
+                  <v-btn dark small color="grey" class="ma-2" @click="UserDelete(row.item)">삭제</v-btn>
                 </div>
             </td>
           </tr>
@@ -75,6 +75,11 @@ import { mapState, mapActions } from "vuex"
         {
           livingLamp40: require("@/assets/livingLamp40.jpg"),
           roomLamp: require("@/assets/roomLamp.jpg"),
+          diningLamp: require("@/assets/diningLamp.jpg"),
+          lightStand: require("@/assets/lightStand.jpg"),
+          ledStand: require("@/assets/ledStand.jpg"),
+          multiPlug: require("@/assets/multiPlug.jpg"),
+          switchTwo: require("@/assets/switchTwo.jpg"),
         },
       }
     },
