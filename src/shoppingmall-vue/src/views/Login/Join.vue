6c72cc11a3a22d21cb7c35749334c368a0ff4067
JaeYoon <taswp@naver.com>
2021-03-19 13:42:07 +0900

    회원가입 입력설명 글자위치 보정

--- src/shoppingmall-vue/src/views/Login/Join.vue
+++ src/shoppingmall-vue/src/views/Login/Join.vue
@@ -5,53 +5,55 @@
               text-align: center;
               width: 100%;
               height: 100%;">
+   <div style="width:300px; margin: 0 auto; text-align:center;">
       <h1 style="text-align:center; margin-bottom:20px;">
           회원가입
       </h1>
 
 
-      <p style="padding-right:250px; margin-bottom:0px;">
+      <p style="margin-bottom:0px; text-align:left;">
           아이디
       </p>
       <p>
         <input style="border: 0.5px solid; width:300px; height: 45px;" v-model="username">
       </p>
 
-      <p style="margin-right:250px; margin-bottom:0px;">
+      <p style="margin-bottom:0px; text-align:left;">
           비밀번호
       </p>
       <p>
         <input style="border: 0.5px solid; width:300px; height: 45px;" v-model="password">
       </p>
 
-      <p style="margin-right:250px; margin-bottom:0px;">
+      <p style="margin-bottom:0px; text-align:left;">
           이름
       </p>
       <p>
         <input style="border: 0.5px solid; width:300px; height: 45px;" v-model="name">
       </p>
 
-      <p style="margin-right:250px; margin-bottom:0px;">
+      <p style="margin-bottom:0px; text-align:left;">
           주소
       </p>
       <p>
         <input style="border: 0.5px solid; width:300px; height: 45px;" v-model="address">
       </p>
 
-      <p style="margin-right:250px; margin-bottom:0px;">
+      <p style="margin-bottom:0px; text-align:left;">
           전화번호
       </p>
       <p>
         <input style="border: 0.5px solid; width:300px; height: 45px;" v-model="phone">
       </p>
 
-      <p style="margin-right:250px; margin-bottom:0px;">
+      <p style="margin-bottom:0px; text-align:left;">
           이메일
       </p>
       <p>
         <input style="border: 0.5px solid; width:300px; height: 45px;" v-model="email">
       </p>
       <v-btn width="300px" height="50px" @click="Join({username, password, name, address, phone, email})">가입하기</v-btn>
+    </div>
   </div>
 </template>
 
